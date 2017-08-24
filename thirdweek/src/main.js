ca203831f3e5ccbaf258cbcfe8b6c0c52266a13d
misshai <miss-hai@yandex.ru>
2017-08-24 15:50:27 +0200

    added several algo runs

--- thirdweek/src/main.js
+++ thirdweek/src/main.js
@@ -1,36 +1,49 @@
+"use strict";
+function createGraph() {
+    let graph = new Graph();
+    data.map((val, index, arr)=> {
+        graph.addVertex(index + 1);
+        val.map((val, i)=> {
+            if (i != 0) graph.addEdge(index + 1, val);
+        });
+    });
+    return graph;
+}
 
-let graph = new Graph();
-data.map((val,index,arr)=>{
-    graph.addVertex(index+1);
-    val.map((val, i)=>{if (i!=0) graph.addEdge(index+1, val);});
-});
+function minCuts(graph) {
 
-
-function minCuts(){
-
-    while(graph.size()>2){
-
-        let randX = Math.floor(Math.random()*graph.size());
+    while (graph.size() > 2) {
+        let randX = Math.floor(Math.random() * graph.size());
         randX = graph.getVertexByIndex(randX);
-        let randY =Math.floor(Math.random()*graph.relationsPerVertex(randX));
+        let randY = Math.floor(Math.random() * graph.relationsPerVertex(randX));
         randY = graph.getRelationsPerVertex(randX)[randY];
         //remove relation
         graph.removeEdge(randX, randY);
         graph.removeEdge(randY, randX);
         //go by string and replace relation
         let arr = graph.getRelationsPerVertex(randY);
-        for(let i of arr){
-            if(randX!=i)
-            graph.addEdge(randX, i);
+        for (let i of arr) {
+            if (randX != i) {
+                graph.addEdge(randX, i);
+                graph.addEdge(i, randX);
+            }
             graph.removeEdge(i, randY);
-            if(randX!=i)
-            graph.addEdge(i, randX  );
         }
         //remove string
         graph.removeVertex(randY);
     }
-    return (graph.relationsPerVertex(graph.getVertexByIndex(0)) == graph.relationsPerVertex(graph.getVertexByIndex(1)))?graph.relationsPerVertex(graph.getVertexByIndex(1)):0;
-
+    return (graph.relationsPerVertex(graph.getVertexByIndex(0)) == graph.relationsPerVertex(graph.getVertexByIndex(1)))
+        ? graph.relationsPerVertex(graph.getVertexByIndex(1)) : 0;
 }
 
-console.log(minCuts())
+function run(){
+    let minResult = 1e100;
+    for (let i = 0;i<1e2;i++){
+        let result = minCuts(createGraph());
+        minResult = Math.min(minResult, result);
+
+    }
+    return minResult;
+}
+
+console.log(run())
